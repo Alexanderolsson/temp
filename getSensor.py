68a2862927650310bbeaa743a2b439c3b444fa70
Alexanderolsson <alexanderolsson95@gmail.com>
2017-10-26 14:42:07 +0200

    Add files via upload

---
+++ getSensor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import MySQLdb #lib för att ansluta till mysql/mariadb
+import time #lib för tid
+import requests #lib för att hämta innehåll från hemsida
+import datetime
+
+conn = MySQLdb.connect(host="localhost", 
+  user="root",
+  passwd="",
+  db="") #Ansluter till databas
+curr = conn.cursor() #Variable för db cursor
+
+nowTime = (time.strftime("%y-%m-%d ") + time.strftime("%H:%M:%S")) #vaiable för tid exempel format 17-20-3 11:24:20 
+
+url = "http://192.168.10.112/arduino/digital/" #Arduino url för rest-server
+getTemp = requests.get(url) #hämtar innehållet på en sida
+sensorValue = getTemp.text #gör om det till text vilket kommer visa värdet för sensorn
+a = sensorValue.replace("\n", "") #Tar bort newline och space för innehållet
+
+sqlAdd = ("""INSERT INTO tempLog (datetime,temperature) VALUES (%s,%s)""", (nowTime,a)) #Variable för att lägga till temperatur och tid
+curr.execute(*sqlAdd) #Kör variablen
+conn.commit() #slutför sql insert
+curr.close() #stänger cursor
+conn.close() #stänger db anslutning
---
+++ project.ino
@@ -0,0 +1,52 @@
+#include<UnoWiFiDevEd.h>
+#include<LiquidCrystal.h>
+#include<Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+LiquidCrystal lcd(12,11,6,5,4,3);
+void setup(){
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  Wifi.begin();
+  sensors.begin();
+}
+
+void loop() {
+  while(Wifi.available()){
+    process(Wifi);
+  }
+  sensors.requestTemperatures();
+  float celsius = sensors.getTempCByIndex(0);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(celsius);
+  lcd.print("c");
+  Serial.println(celsius);
+  delay(2000);
+}
+
+void process(WifiData client){
+  String command = client.readStringUntil('/');
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+}
+
+void digitalCommand(WifiData client) {
+  
+  if (client.read() == '/') {
+  }
+  float celsius = sensors.getTempCByIndex(0);
+  client.println("HTTP/1.1 200 OK\n");
+  client.println(celsius);
+  client.println(EOL);    //char terminator
+
+}
+
